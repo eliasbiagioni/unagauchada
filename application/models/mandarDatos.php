aecfd726a54975b6c2e76e5c976ba75a70133f25
Fernando <fer.borgognoni@gmail.com>
2017-05-23 21:56:54 -0300

    Prueba

--- application/models/mandarDatos.php
+++ application/models/mandarDatos.php
@@ -1,2 +1,3 @@
 <?php
+	#Acá debería manejarse la información del formulario de Comprar Créditos para agregarle el o los créditos comprados al usuario en la BBDD.
 ?>
---
+++ application/views/imag.php
@@ -0,0 +1,6 @@
+<html>
+    <title></title>
+    <body>
+        <img src="data:image/<?= $par_image['extension_foto'] ?>; base64,<?= $par_image['encode'] ?>" >
+    </body>
+</html>
