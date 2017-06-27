452e859e97d5bcbdd6d5ca8595b6e7a0544d1100
eliasbiagioni <eliasbiagioni96@hotmail.com>
2017-06-27 17:26:18 -0300

    Aca esta todo
    Esto es despues de que desapareciera el editar perfil

---
+++ application/views/realizarPregunta.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="http://localhost/unagauchada/css/imagen_principal.css" rel="stylesheet" type="text/css" />
+        <link href="http://localhost/unagauchada/css/sesionIniciada.css" rel="stylesheet" type="text/css" /> 
+        <link href="http://localhost/unagauchada/css/formulario_registro.css" rel="stylesheet" type="text/css" /> 
+        <script type="text/javascript" charset="utf-8" src="http://localhost/unagauchada/js/validarDatos.js"></script> 
+        <title>Una gauchada</title>
+    </head>
+    <body>
+        <img src="http://localhost/unagauchada/images/unagauchada.png" class="size_image" alt=""/>
+    <div class="barraInicial longitud">
+        <div class="bienvenido">
+                Bienvenido al sitio
+        </div>
+        <ul id="button">
+            <li><a href="<?= base_url() ?>publicar_gauchada/volverAInicio">Volver a la pagina de inicio</a></li>
+        </ul>
+    </div>
+    <hr class="longitud">
+    <h1>Realizar pregunta</h1>
+    <div class="contenedorRegistroGauchada letra" >
+        <?php 
+            echo form_open('verGauchadaCompleta/mandarPregunta','name="formPreguntas" onsubmit="return noVacio()""'); 
+            echo form_textarea($area_pregunta);
+            echo '<span>'.form_error('pregunta').'</span>';
+            echo br();
+            ?>
+            <input type="hidden" name="id_favor" value="<?php echo $id_favor ?>">
+            <input type="hidden" name="id_usuario" value="<?php echo $id_usuario ?>">
+            <?php
+            echo form_submit('','Enviar pregunta');
+            echo form_close();
+        ?>
+    </div>
+    </body>
+</html>
